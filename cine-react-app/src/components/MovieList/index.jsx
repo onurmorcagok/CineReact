d63a7423de91b31dc60681497da705edd67ef726
Onur MORCAGÖK <55251990+onurmorcagok@users.noreply.github.com>
2022-01-22 23:24:56 +0300

    Movielist component edited

--- cine-react-app/src/components/MovieList/index.jsx
+++ cine-react-app/src/components/MovieList/index.jsx
@@ -1,5 +1,15 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
+
+import "./index.scss";
+
+import { SwiperSlide, Swiper } from "swiper/swiper-react";
+import { Link } from "react-router-dom";
+
+import Button from "../Button/index";
+
+import tmdbApi, { category } from "../../api/tmdbApi";
+import apiConfig from "../../api/apiConfig";
 
 const MovieList = (props) => {
   return <div></div>;
