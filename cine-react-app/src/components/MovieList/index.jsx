61696439b756ebe1e427c71c17d2fe0cf186ca15
Onur MORCAGÖK <55251990+onurmorcagok@users.noreply.github.com>
2022-01-23 00:43:27 +0300

    Homepage slides edited

--- cine-react-app/src/components/MovieList/index.jsx
+++ cine-react-app/src/components/MovieList/index.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 import "./index.scss";
 
-import { SwiperSlide, Swiper } from "swiper/swiper-react";
+import { SwiperSlide, Swiper } from "swiper/react";
 import { Link } from "react-router-dom";
 
 import Button from "../Button/index";
@@ -12,7 +12,40 @@ import tmdbApi, { category } from "../../api/tmdbApi";
 import apiConfig from "../../api/apiConfig";
 
 const MovieList = (props) => {
-  return <div></div>;
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const getList = async () => {
+      let response = null;
+      const params = {};
+
+      if (props.type !== "similar") {
+        switch (props.category) {
+          case category.movie:
+            response = await tmdbApi.getMoviesList(props.type, { params });
+            break;
+          default:
+            response = await tmdbApi.getTvList(props.type, { params });
+        }
+      } else {
+        response = await tmdbApi.similar(props.category, props.id);
+      }
+      setItems(response.results);
+    };
+    getList();
+  }, []);
+
+  return (
+    <div className="movie-list">
+      <Swiper grabCursor={true} spaceBetween={10} slidesPerView={"auto"}>
+        {items.map((item, index) => (
+          <SwiperSlide key={index}>
+            <img src={apiConfig.w500Image(item.poster_path)} alt="" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 };
 
 MovieList.propTypes = {
--- cine-react-app/src/pages/Home.jsx
+++ cine-react-app/src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { OutlineButton } from "../components/Button";
 
+import { OutlineButton } from "../components/Button";
 import HeroSlider from "../components/Hero-Slider";
+import MovieList from "../components/MovieList";
+
+import { category, movieType, tvType } from "../api/tmdbApi";
 
 function Home() {
   return (
@@ -16,6 +19,43 @@ function Home() {
               <OutlineButton className="small">View More</OutlineButton>
             </Link>
           </div>
+          <MovieList category={category.movie} type={movieType.popular} />
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="section mb-3">
+          <div className="section__header mb-2">
+            <h2>Top Rated Movies</h2>
+            <Link to="/movie">
+              <OutlineButton className="small">View More</OutlineButton>
+            </Link>
+          </div>
+          <MovieList category={category.movie} type={movieType.top_rated} />
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="section mb-3">
+          <div className="section__header mb-2">
+            <h2>Trending TV Series</h2>
+            <Link to="/movie">
+              <OutlineButton className="small">View More</OutlineButton>
+            </Link>
+          </div>
+          <MovieList category={category.tv} type={tvType.popular} />
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="section mb-3">
+          <div className="section__header mb-2">
+            <h2>Top Rated TV Series</h2>
+            <Link to="/movie">
+              <OutlineButton className="small">View More</OutlineButton>
+            </Link>
+          </div>
+          <MovieList category={category.tv} type={tvType.top_rated} />
         </div>
       </div>
     </>
