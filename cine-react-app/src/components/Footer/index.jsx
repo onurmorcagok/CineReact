65b611ad57a424a29cb2031b58d1bd617b3a71ad
Onur MORCAGÖK <55251990+onurmorcagok@users.noreply.github.com>
2022-01-27 23:12:28 +0300

    Footer content fixed

--- cine-react-app/src/components/Footer/index.jsx
+++ cine-react-app/src/components/Footer/index.jsx
@@ -13,27 +13,26 @@ function Footer() {
       style={{ backgroundImage: `url(${footerBackground})` }}
     >
       <div className="footer__content container">
-        <div className="footer__content__logo">Onur MORCAGÖK</div>
+        <div className="footer__content__logo">
+          <h2>©2022 All Rights Reserved</h2>
+        </div>
         <div className="footer__content__menus">
           <div className="footer__content__menu">
             <Link to="/">Home</Link>
-            <Link to="/">Home</Link>
-            <Link to="/">Home</Link>
-            <Link to="/">Home</Link>
+            <Link to="/">Contact us</Link>
+            <Link to="/">Term of services</Link>
+            <Link to="/">About us</Link>
           </div>
-
           <div className="footer__content__menu">
-            <Link to="/">Home</Link>
-            <Link to="/">Home</Link>
-            <Link to="/">Home</Link>
-            <Link to="/">Home</Link>
+            <Link to="/">Live</Link>
+            <Link to="/">FAQ</Link>
+            <Link to="/">Premium</Link>
+            <Link to="/">Pravacy policy</Link>
           </div>
-
           <div className="footer__content__menu">
-            <Link to="/">Home</Link>
-            <Link to="/">Home</Link>
-            <Link to="/">Home</Link>
-            <Link to="/">Home</Link>
+            <Link to="/">You must watch</Link>
+            <Link to="/">Recent release</Link>
+            <Link to="/">Top IMDB</Link>
           </div>
         </div>
       </div>
