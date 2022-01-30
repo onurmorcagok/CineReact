53d8ee1bbfed5738d31b52a1d6502d284b91e2bb
Onur MORCAGÖK <55251990+onurmorcagok@users.noreply.github.com>
2022-01-30 23:18:02 +0300

    Grid component edited and input component created

---
+++ cine-react-app/src/components/Input/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./index.scss";
+
+const Input = (props) => {
+  return (
+    <input
+      type={props.type}
+      placeholder={props.placeholder}
+      value={props.value}
+      onChange={props.onChange ? (e) => props.onChange(e) : null}
+    />
+  );
+};
+
+export default Input;
--- cine-react-app/src/components/MovieGrid/index.jsx
+++ cine-react-app/src/components/MovieGrid/index.jsx
@@ -1,10 +1,11 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 import "./index.scss";
 
 import MovieCard from "../MovieCard";
 import { useParams } from "react-router-dom";
 import tmdbApi, { category, movieType, tvType } from "../../api/tmdbApi";
+import { OutlineButton } from "../Button";
 
 const MovieGrid = (props) => {
   const [items, setItems] = useState([]);
@@ -37,12 +38,48 @@ const MovieGrid = (props) => {
     };
     getList();
   }, [props.category, keyword]);
+
+  const loadMore = async () => {
+    let response = null;
+    if (keyword === undefined) {
+      const params = {
+        page: page + 1,
+      };
+      switch (props.category) {
+        case category.movie:
+          response = await tmdbApi.getMoviesList(movieType.upcoming, {
+            params,
+          });
+          break;
+        default:
+          response = await tmdbApi.getTvList(tvType.popular, { params });
+      }
+    } else {
+      const params = {
+        query: keyword,
+        page: page + 1,
+      };
+      response = await tmdbApi.search(props.category, { params });
+    }
+    setItems([...items, ...response.results]);
+    setPage(page + 1);
+  };
+
   return (
-    <div className="movie-grid">
-      {items.map((item, index) => (
-        <MovieCard key={index} category={props.category} item={item} />
-      ))}
-    </div>
+    <React.Fragment>
+      <div className="movie-grid">
+        {items.map((item, index) => (
+          <MovieCard key={index} category={props.category} item={item} />
+        ))}
+      </div>
+      {page < totalPage ? (
+        <div className="movie-grid__loadmore">
+          <OutlineButton className="small" onClick={loadMore}>
+            Load More
+          </OutlineButton>
+        </div>
+      ) : null}
+    </React.Fragment>
   );
 };
 
