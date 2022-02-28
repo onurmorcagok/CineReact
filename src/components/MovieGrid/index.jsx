d79ac05f99f0b7db3f267a995205e27c17d0afbe
Onur MORCAGÖK <55251990+onurmorcagok@users.noreply.github.com>
2022-02-28 21:59:31 +0300

    Search component history fixed

--- src/components/MovieGrid/index.jsx
+++ src/components/MovieGrid/index.jsx
@@ -94,7 +94,7 @@ const MovieSearch = (props) => {
   const [keyword, setKeyword] = useState(props.keyword ? props.keyword : "");
   const goToSearch = useCallback(() => {
     if (keyword.trim().length > 0) {
-      history.push(`${category[props.category]}/search/${keyword}`);
+      history.push(`/${category[props.category]}/search/${keyword}`);
     }
   }, [keyword, props.category, history]);
 
